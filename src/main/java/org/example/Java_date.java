67bce9c10ba01e00cfa1a389108b63d292459d09
git-spec <inkyfischer@gmail.com>
2025-04-11 16:42:03 +0200

    feat: Created conversion and chrono unit for time.

--- src/main/java/org/example/Java_date.java
+++ src/main/java/org/example/Java_date.java
@@ -7,7 +7,10 @@ package org.example;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 /**
  *
@@ -32,5 +35,36 @@ public class Java_date {
         // Instant - international date with time (Zulu time)
         Instant instant = Instant.now();
         System.out.println(instant);
+
+        // Change Instant into LocalDateTime
+        ZoneId zoneIdLocal = ZoneId.systemDefault(); // local timezone
+        ZoneId zoneIdZone = ZoneId.of("Asia/Tokyo");   // selected timezone
+        LocalDateTime localDateTimeTokyo = LocalDateTime.ofInstant(instant, zoneIdZone);
+        System.out.println(localDateTimeTokyo);
+
+        // Change LocalDateTime into Instant
+        Instant instantLocal = localDateTime.atZone(zoneIdLocal).toInstant();
+        System.out.println(instantLocal);
+
+        // Formatting date and time
+        // d - day, E - weekday, M - month, y - jear, H - hour, m - minute, s- second, n - nano
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EE, dd.MMMM.yyyy : HH:mm:ss");
+        String formattedDate = localDateTime.format(formatter);
+        System.out.println(formattedDate);
+
+        // Change String into LocalDateTime
+        String time = "12.08.2024 : 11:11:11";
+        DateTimeFormatter newFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy : HH:mm:ss");
+        LocalDateTime stringToLocalDateTime = LocalDateTime.parse(time, newFormatter);
+        System.out.println(stringToLocalDateTime);
+
+        // Difference between two point in time
+        LocalDateTime start = LocalDateTime.of(2025, 02, 01, 11, 11);
+        LocalDateTime end = LocalDateTime.of(2025, 03, 01, 11, 11);
+
+        long diffHour = ChronoUnit.HOURS.between(start, end);
+        System.out.println(diffHour);
+        long diffSeconds = ChronoUnit.SECONDS.between(start, end);
+        System.out.println(diffSeconds);
     }
 }
